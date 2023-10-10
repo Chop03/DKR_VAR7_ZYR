4532467f2d5929ee08a4767d4c580ba7522d328a
Chop03 <144237862+Chop03@users.noreply.github.com>
2023-10-10 10:03:53 +0300

    Add files via upload

---
+++ ДКР_ВАР7_2.pas
@@ -0,0 +1,12 @@
+﻿var x,y:real;
+begin
+    x:= -12;
+    while x<=-2 do begin
+      if (x < -10) then y:=sin(x)/sin(x)
+      else if (x<-4) then y:=ln(x) * Power(x, 1/3) + x
+      else y:=log10(x) / -x + -x;
+      if y<>y then writeln('нет решения')
+      else writeln(y);      
+      x:=x+0.2;
+    end;
+end.
